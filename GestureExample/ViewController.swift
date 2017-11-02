bfa1b980ed1dcd71340ffe1c1ac7ee8ff0a3b1e6
Gregory Rudolph <rudi@nightmare.haus>
2017-11-02 18:29:16 -0400

    Initial Commit

---
+++ GestureExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureExample
+//
+//  Created by Gregory Rudolph on 11/2/17.
+//  Copyright © 2017 Gregory Rudolph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
